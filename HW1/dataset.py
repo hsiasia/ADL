b76c6ea5de84baf393d4236c5ca6defd4f025457
hsiasia <pompompurin8787@gmail.com>
2022-10-09 10:36:19 +0800

    <HW1> add slot dataset, renew model annotation, add slot basic lstm model

--- HW1/dataset.py
+++ HW1/dataset.py
@@ -1,10 +1,10 @@
+from random import sample
 from typing import List, Dict
 
+import torch
 from torch.utils.data import Dataset
 
-from utils import Vocab
-
-import torch
+from utils import Vocab, pad_to_len
 
 class SeqClsDataset(Dataset):
     def __init__(
@@ -33,6 +33,7 @@ class SeqClsDataset(Dataset):
 
     def collate_fn(self, samples: List[Dict]) -> Dict:
         # TODO: implement collate_fn
+        samples.sort(key=lambda sample: len(sample['text'].split()), reverse=True)
         batch = {}
         batch['id'] = [sample['id'] for sample in samples]
         batch['text'] = [sample['text'].split() for sample in samples]
@@ -43,12 +44,12 @@ class SeqClsDataset(Dataset):
 
         if 'intent' in samples[0].keys():
             batch['intent'] = [self.label2idx(sample['intent']) for sample in samples]
-            batch['intent'] = torch.tensor(batch['intent'])
-        # else:
-            # batch['intent'] = torch.zero(len(samples), dtype=torch.long)
+        else:
+            batch['intent'] = torch.zeros(len(samples), dtype=torch.long)
+        
+        batch['intent'] = torch.tensor(batch['intent'])
         
         return batch
-        raise NotImplementedError
 
     def label2idx(self, label: str):
         return self.label_mapping[label]
@@ -59,7 +60,56 @@ class SeqClsDataset(Dataset):
 
 class SeqTaggingClsDataset(SeqClsDataset):
     ignore_idx = -100
+    def __init__(
+        self,
+        data: List[Dict],
+        vocab: Vocab,
+        label_mapping: Dict[str, int],
+        max_len: int,
+    ):
+        self.data = data
+        self.vocab = vocab
+        self.label_mapping = label_mapping
+        self._idx2label = {idx: tag for tag, idx in self.label_mapping.items()}
+        self.max_len = max_len
 
-    def collate_fn(self, samples):
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index) -> Dict:
+        instance = self.data[index]
+        return instance
+
+    @property
+    def num_classes(self) -> int:
+        return len(self.label_mapping)
+
+    def collate_fn(self, samples: List[Dict]) -> Dict:
         # TODO: implement collate_fn
-        raise NotImplementedError
+        samples.sort(key=lambda sample: len(sample['tokens']), reverse=True)
+        batch = {}
+        batch['id'] = [sample['id'] for sample in samples]
+        batch['tokens'] = [sample['tokens'] for sample in samples]
+        batch['len'] = torch.tensor([min(len(token), self.max_len) for token in batch['tokens']])
+
+        batch['tokens'] = self.vocab.encode_batch(batch['tokens'], self.max_len)
+        batch['tokens'] = torch.tensor(batch['tokens'])
+
+        if 'tags' in samples[0].keys():
+            batch['tags'] = [[self.label2idx(tag) for tag in sample['tags']] for sample in samples]
+            batch['tags'] = pad_to_len(batch['tags'], self.max_len, -1)
+        else:
+            # ? 
+            batch['tags'] = [[0] * self.max_len] * len(samples)
+
+        batch['tags'] = torch.tensor(batch['tags'])
+        # ?
+        batch['mask'] = batch['tokens'].gt(0).float()
+
+        return batch
+
+    def label2idx(self, label: str):
+        return self.label_mapping[label]
+
+    def idx2label(self, idx: int):
+        return self._idx2label[idx]
--- HW1/model.py
+++ HW1/model.py
@@ -1,4 +1,5 @@
-from typing import Dict
+import profile
+from typing import Dict, List
 
 import torch
 import torch.nn as nn
@@ -18,25 +19,22 @@ class SeqClassifier(torch.nn.Module):
     ) -> None:
         super(SeqClassifier, self).__init__()
         self.embed = Embedding.from_pretrained(embeddings, freeze=False)
-        self.embed_dim = embeddings.size(1)
         # TODO: model architecture
+        self.embed_dim = embeddings.size(1)
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.dropout = dropout
         self.bidirectional = bidirectional
         self.num_class = num_class
 
+        # 300, 512, 2, 0.1, 2
         self.lstm = nn.LSTM(input_size = self.embed_dim, hidden_size = self.hidden_size, num_layers = self.num_layers,
-                     dropout = self.dropout, bidirectional = self.bidirectional, batch_first = True)
-        # self.classifier = nn.Sequential(
-        #     nn.Dropout(dropout),
-        #     nn.Linear(self.hidden_size * 2, self.num_class)
-        # )
+                            dropout = self.dropout, bidirectional = self.bidirectional, batch_first = True)
         self.classifier = nn.Sequential(nn.Dropout(self.dropout),
-                                        nn.Linear(in_features = self.hidden_size * 2, out_features = self.hidden_size * 2),
-                                        # nn.Relu(),
+                                        nn.Linear(in_features = self.hidden_size * 2, out_features = self.hidden_size),
+                                        nn.ReLU(),
                                         nn.Dropout(self.dropout),
-                                        nn.Linear(in_features = self.hidden_size * 2, out_features = self.num_class))
+                                        nn.Linear(in_features = self.hidden_size, out_features = self.num_class))
 
     @property
     def encoder_output_size(self) -> int:
@@ -47,109 +45,139 @@ class SeqClassifier(torch.nn.Module):
         # TODO: implement model forward
         output_dict = {}
 
-        # batch:  {'id': ['train-11240' ..(id*128).. 'train-621'], 
-        #         'text': tensor([[3892, 5790, 4329,  ..(word*128)..,    0,    0,    0] ..(sentence*128).. [ 449, 4931, 3684,  ..(word*128)..,    0,    0,    0]]), 
-        #         'len': tensor([12 ..(word_in_a_sentence*128)..  6]), 
-        #         'intent': tensor([27 ..(intent2idx*128).. 117])}
-        # batch['text']: torch.Size([128, 128]) / [batch_size max_word_in_a_sentence]
-        # batch['intent']: torch.Size([128]) / [batch_size]
-        text, intent = batch['text'], batch['intent']
+        # batch:  {'id': ['train-11240', ..(*128).., 'train-621'], 
+        #          'text': tensor([[3892, ..(*128).., 0], ..(*128).., [449, ..(*128).., 0]]), 
+        #          'len': tensor([12, ..(*128).., 6]), 
+        #          'intent': tensor([27, ..(*128).., 117])}
+        text, intent = batch['text'], batch['intent'] # [batch_size = 128, max_len = 128] # [batch_size = 128]
         
-        # text_embedded: torch.Size([128, 128, 300]) / [batch_size, max_word_in_a_sentence, embed_dim]
-        # text_embedded[128][128][300]
-        # text_embedded: [[[-0.1130,  0.3207, -0.5438 ..word3embd*300.. 0.1905,  0.3545,  0.1077],
-        #                   ..word*128..,
-        #                  [-0.4820,  0.3705,  0.3682 ..*300.. -0.0015,  0.1438, -0.1723]],
-        #                 ..sentence*128..,
-        #                 [[-0.7481,  0.8208,  0.0107 ..*300..  0.2947, -0.0981,  0.8008],
-        #                   ..word*128..,
-        #                  [-0.4820,  0.3705,  0.3682 ..*300.. -0.0015,  0.1438, -0.1723]]],
-        text_embedded = self.embed(text)
+        # text_embedded: [[[-0.1130, ..(*300)..,  0.1077], ..(*128).., [-0.4820, ..(*300).., -0.1723]],
+        #                 ..(*128)..,
+        #                 [[-0.7481, ..(*300)..,  0.8008], ..(*128).., [-0.4820, ..(*300).., -0.1723]]],
+        text_embedded = self.embed(text) # [batch_size = 128, max_len = 128, embed_dim = 300]
 
-        # ?
-        # pack_padded_sequence to dymanicly address each batch's len
-        # PackedSequence(data=tensor([[-0.1038,  0.2065, -0.1937 ..*300.. -0.2407,  0.3086,  0.1792],
-        #                               ..*?..,
-        #                             [-0.3201,  0.8325,  0.6346 ..*300.. -0.1604, -0.0658,  0.0997]]), 
-        #               batch_sizes=tensor([128, 126, ..max batch['len'].. 2,   1]), 
-        #               sorted_indices=tensor([ 12, 120 ..*128.. 24,  78]), 
-        #               unsorted_indices=tensor([ 2,  90, ..*128.. 35,  76]))
-        # data: torch.Size([?, 300]) gard
-        packed_text = nn.utils.rnn.pack_padded_sequence(text_embedded, batch['len'], batch_first=True, enforce_sorted=False)
+        # PackedSequence(data=tensor([[-0.1038, ..(*300).., 0.1792], ..(*size_after_packed).., [-0.3201, ..(*300).., 0.0997]]), 
+        #                batch_sizes=tensor([128, ..(*mini_batch_max_len).., 1]), 
+        #                sorted_indices=tensor([12, ..(*128).., 78]), 
+        #                unsorted_indices=tensor([2, ..(*128).., 76]))
+        packed_text = nn.utils.rnn.pack_padded_sequence(text_embedded, batch['len'], batch_first=True) # [size_after_packed, embed_dim = 300]
         self.lstm.flatten_parameters()
 
-        # ?
-        # PackedSequence(data=tensor([[-2.6123e-03,  3.0056e-02,  2.3325e-03,  ..1024..,  4.2309e-02, -5.2573e-02, -7.1546e-03],
-        #                               ..?..,
-        #                             [-5.4017e-03,  5.9926e-03,  9.0346e-03,  ...,  1.2922e-02, -2.9177e-02, -5.2137e-03]]), 
+        # PackedSequence(data=tensor([[-2.6123e-03, ..(*1024).., -7.1546e-03], ..(*size_after_packed).., [-5.4017e-03, ..(*1024).., -5.2137e-03]]), 
         #                batch_sizes=as packed, 
         #                sorted_indices=as packed, 
         #                unsorted_indices=as packed)
-        # data: torch.Size([?, 1024]) gard
-        # hidden: torch.Size([4, 128, 512]) grad
-        # hidden: tensor([
-        # [[ 5.1340e-02, -8.9667e-02, -4.2291e-02,  ..*512.., -3.3602e-02, -2.5190e-02, -2.1012e-02],
-        #  ..*128..,
-        #  [ 4.0590e-02, -6.4623e-02, -3.6845e-04,  ..., -4.8889e-02, 5.1251e-02, -9.3412e-02]],
-        # [[ 1.6498e-02,  3.1234e-02, -2.2041e-02,  ..., -2.2359e-02, -5.0190e-02, -6.6134e-02],
-        #  ..*128..,
-        #  [-3.4370e-02,  1.9222e-02, -3.2772e-02,  ...,  1.7065e-02, -4.5621e-02, -3.4005e-02]],
-        # [[-8.7334e-03,  2.8085e-02,  2.5695e-03,  ..., -2.6970e-02, 8.2791e-03, -1.3702e-02],
-        #  ..*128..,
-        #  [ 9.8418e-03,  2.7368e-02,  1.9901e-02,  ..., -5.4476e-02, -8.0476e-03, -2.1952e-02]],
-        # [[-6.5971e-02,  1.2094e-02, -2.9507e-02,  ...,  9.6665e-03, 3.3974e-02, -6.0133e-04],
-        #  ..*128..,
-        #  [-5.4275e-02,  3.0708e-02, -2.2902e-02,  ...,  2.9815e-05, 3.8676e-02, -7.6376e-03]]]
+        # hidden: tensor([[[5.1340e-02, ..(*512).., -2.1012e-02], ..(*128).., [4.0590e-02, ..(*512).., -9.3412e-02]],
+        #                 ..(*4)..
+        #                 [[-6.5971e-02, ..(*512).., -6.0133e-04], ..(*128).., [-5.4275e-02, ..(*512).., -7.6376e-03]]]
         # cell: as hidden
-        output_packed, (hidden, cell) = self.lstm(packed_text)
+        output_packed, (hidden, cell) = self.lstm(packed_text) # [size_after_packed, output_cells = hidden_size*bidirectinal] # [all_layers = num_layers*bidirectinal, batch_size = 128, hidden_size = 512]
 
-        # output: torch.Size([128, max_word_in_sentence_this_batch, 1024]) grad / [batch_size, max_word_in_sentence_this_batch, hid_dim]
-        # output: [[[-0.0214, -0.0208, -0.0008 ..*1024.. -0.0178,  0.0170,  0.0040],
-        #           ..*max_word_in_sentence_this_batch..,
-        #           [ 0.0000,  0.0000,  0.0000 ..*1024..  0.0000,  0.0000,  0.0000]],
-        #          ..*128..,
-        #          [[-0.0164, -0.0189,  0.0013 ..*1024.. -0.0185,  0.0105,  0.0133],
-        #           ..*max_word_in_sentence_this_batch..,
-        #           [ 0.0000,  0.0000,  0.0000 ..*1024..  0.0000,  0.0000,  0.0000]]],
-        # output_length: torch.Size([128]) / [max_word_in_sentence_this_batch]
-        # [ 6 ..length*128.. 13]
-        output, output_length = nn.utils.rnn.pad_packed_sequence(output_packed, batch_first=True)
+        # output: [[[-0.0214, ..(*1024).., 0.0040], ..(*mini_batch_max_len).., [ 0.0000, ..(*1024).., 0.0000]],
+        #          ..(*128)..,
+        #          [[-0.0164, ..(*1024).., 0.0133], ..(*mini_batch_max_len).., [ 0.0000, ..(*1024).., 0.0000]]],
+        output, _ = nn.utils.rnn.pad_packed_sequence(output_packed, batch_first=True) # [batch_size = 128, mini_batch_max_len, output_cells = hidden_size*bidirectinal]
         
-        # hidden[-1]:  torch.Size([128, 512])
-        # hidden[cat]:  torch.Size([128, 1024])
         # -1 the last, -2 the second last
         if self.bidirectional:
-            hidden = torch.cat((hidden[-1], hidden[-2]), axis=-1) 
-        else:
-            hidden = hidden[-1]
+            hidden = torch.cat((hidden[-1], hidden[-2]), axis=-1) # [batch_size = 128, hidden_size*2 = 1024]
 
         # the result of model save as list
-        pred_logits = [self.classifier(hidden)]
-
-        output_dict['pred_logits'] = pred_logits
+        pred_logits = self.classifier(hidden)
+        # output_dict['pred_logits'] = [pred_logits]
         
-        # pred_logits[-1].max(1, keepdim=True)[1]: tensor([128, 1])
-        # torch.return_types.max(
-        #   values=tensor([[0.0596], [0.0496] ..*128.. [0.0481], [0.0532]], grad_fn=<MaxBackward0>),
-        #   indices=tensor([[122], [ 44], ..*128.. , [ 13], [ 35]]))
-
-        # list[-1] = list[0] (the first element in list)
-        # 1. max(1, keepdim=True) 1 means select the max and index, True makes output shape same as pred_logits[-1]
-        # 2. [1] and select second element (label's index)
-        # 3. reshape(-1) row to column
-        # output_dict['pred_labels']: torch.Size([128])
-        output_dict['pred_labels'] = pred_logits[-1].max(1, keepdim=True)[1].reshape(-1)
+        # 1 select the max and indices from each row in matrix [num_classes], True makes output shape same as pred_logits[-1]
+        # select second element (label's indices)
+        # reshape row to column
+        # pred_logits.max(1, keepdim=True): torch.return_types.max(values=tensor([[0.0596], ..(*128).., [0.0532]],
+        #                                                          indices=tensor([[122], ..(*128).., [35]]))
+        # pred_logits.max(1, keepdim=True)[1]: [batch_size = 128, 1]
+        # output_dict['pred_labels'] = pred_logits.max(1, keepdim=True)[1].reshape(-1) # [batch_size = 128]
+        output_dict['pred_labels'] = torch.argmax(pred_logits, dim=-1) # [batch_size = 128]
 
         # caculate the loss
-        # intent.long():　torch.Size([128])
-        # pred_logits[-1]: torch.Size([128, 150]) grad
-        output_dict['loss'] = F.cross_entropy(pred_logits[-1], intent.long())
+        # intent.long():　[batch_size = 128]
+        # pred_logits: [batch_size = 128, num_classes = 150]
+        output_dict['loss'] = F.cross_entropy(pred_logits, intent.long())
 
         return output_dict
+
+
+class SeqTagger(nn.Module):
+    def __init__(
+        self,
+        embeddings: torch.tensor,
+        hidden_size: int,
+        num_layers: int,
+        dropout: float,
+        bidirectional: bool,
+        num_classes: int,
+    ) -> None:
+        super(SeqTagger, self).__init__()
+        self.embed = Embedding.from_pretrained(embeddings, freeze=False)
+        # model architecture
+        self.embed_dim = embeddings.size(1)
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.dropout = dropout
+        self.bidirectional = bidirectional
+        self.num_classes = num_classes
+
+        # 300, 512, 2, 0.1, 2
+        self.lstm = nn.LSTM(input_size = self.embed_dim, hidden_size = self.hidden_size, num_layers = self.num_layers,
+                            dropout = self.dropout, bidirectional = self.bidirectional, batch_first = True)
+        self.classifier = nn.Sequential(nn.Dropout(self.dropout),
+                                        nn.Linear(in_features = self.hidden_size * 2, out_features = self.hidden_size),
+                                        nn.ReLU(),
+                                        nn.Dropout(self.dropout),
+                                        nn.Linear(in_features = self.hidden_size, out_features = self.num_classes))
+
+    @property
+    def encoder_output_size(self) -> int:
+        # TODO: calculate the output dimension of rnn
         raise NotImplementedError
 
+    def getIdx(self, tokens_len):
+        # put indexi for t_len times, and concat
+        batch_idx = torch.cat([torch.full((t_len,), i) for i, t_len in enumerate(tokens_len)])
+        # put 0 to t_len, and concat
+        tok_idx = torch.cat([torch.arange(0, t_len) for t_len in tokens_len])
+        return batch_idx, tok_idx
 
-class SeqTagger(SeqClassifier):
     def forward(self, batch) -> Dict[str, torch.Tensor]:
         # TODO: implement model forward
-        raise NotImplementedError
+        output_dict = {}
+
+        tokens = batch['tokens'] # [batch_size = 128, max_len = 128]
+
+        tokens_embedded = self.embed(tokens) # [batch_size = 128, max_len = 128, embed_dim = 300]
+
+        # all batch data delete the pad part
+        # batch_first for lstm time step input (order by batch first status)
+        packed_tokens = nn.utils.rnn.pack_padded_sequence(tokens_embedded, batch['len'], batch_first=True) # [size_after_packed, embed_dim = 300]
+        
+        self.lstm.flatten_parameters()
+        # (stil order by word status)
+        output_packed, _ = self.lstm(packed_tokens) # [size_after_packed, output_cells = hidden_size*bidirectinal]
+        
+        # all batch data with pad part (but it means mini_batch_max_len)
+        # batch_first for fc input (order by batch first status)
+        output, _ = nn.utils.rnn.pad_packed_sequence(output_packed, batch_first=True) # [batch_size = 128, mini_batch_max_len, output_cells = hidden_size*bidirectinal]
+        
+        pred_logits = self.classifier(output) # [batch_size = 128, mini_batch_max_len, num_classes = 9]
+
+        # -1 select the max and indices from each row in matrix [mini_batch_max_len*num_classes], True makes output shape same as pred_logits
+        # select second element (label's indices)
+        # output_dict['pred_logits'] = pred_logits.max(-1, keepdim=True)[1] # [batch_size = 128, mini_batch_max_len, 1]
+        output_dict['pred_labels'] = pred_logits.max(-1, keepdim=True)[1].squeeze(2) # [batch_size = 128, mini_batch_max_len]
+        
+        # caculate the loss
+        batch['tags'] = batch['tags'][:, :output.size(1)] # [batch_size = 128, max_len = 128] -> [batch_size = 128, mini_batch_max_len]
+        # get all data, token index in batch
+        idx = self.getIdx(batch['len'])
+        # ? how to 2 tensor turn to 1 index
+        # pred_logits[idx]: [size_after_packed, num_classes]
+        # batch['tags'][idx]: [size_after_packed]
+        output_dict['loss'] = F.cross_entropy(pred_logits[idx], batch['tags'][idx])
+
+        return output_dict
